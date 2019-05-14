7a6c76d806f10d83c9682480afef53e0f2de551d
stratex <aqja88@hotmail.com>
2019-05-14 21:22:16 +1200

    Logic added

---
+++ GlobalWordCount/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace GlobalWordCount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ckWindows = new System.Windows.Forms.CheckBox();
+            this.ckShift = new System.Windows.Forms.CheckBox();
+            this.ckControl = new System.Windows.Forms.CheckBox();
+            this.ckAlt = new System.Windows.Forms.CheckBox();
+            this.txtChar = new System.Windows.Forms.TextBox();
+            this.cmdSave = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ckWindows);
+            this.groupBox1.Controls.Add(this.ckShift);
+            this.groupBox1.Controls.Add(this.ckControl);
+            this.groupBox1.Controls.Add(this.ckAlt);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(234, 114);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Modifiers";
+            // 
+            // ckWindows
+            // 
+            this.ckWindows.AutoSize = true;
+            this.ckWindows.Location = new System.Drawing.Point(7, 88);
+            this.ckWindows.Name = "ckWindows";
+            this.ckWindows.Size = new System.Drawing.Size(70, 17);
+            this.ckWindows.TabIndex = 3;
+            this.ckWindows.Text = "Windows";
+            this.ckWindows.UseVisualStyleBackColor = true;
+            // 
+            // ckShift
+            // 
+            this.ckShift.AutoSize = true;
+            this.ckShift.Location = new System.Drawing.Point(7, 65);
+            this.ckShift.Name = "ckShift";
+            this.ckShift.Size = new System.Drawing.Size(47, 17);
+            this.ckShift.TabIndex = 2;
+            this.ckShift.Text = "Shift";
+            this.ckShift.UseVisualStyleBackColor = true;
+            // 
+            // ckControl
+            // 
+            this.ckControl.AutoSize = true;
+            this.ckControl.Location = new System.Drawing.Point(6, 42);
+            this.ckControl.Name = "ckControl";
+            this.ckControl.Size = new System.Drawing.Size(59, 17);
+            this.ckControl.TabIndex = 1;
+            this.ckControl.Text = "Control";
+            this.ckControl.UseVisualStyleBackColor = true;
+            // 
+            // ckAlt
+            // 
+            this.ckAlt.AutoSize = true;
+            this.ckAlt.Location = new System.Drawing.Point(6, 19);
+            this.ckAlt.Name = "ckAlt";
+            this.ckAlt.Size = new System.Drawing.Size(38, 17);
+            this.ckAlt.TabIndex = 0;
+            this.ckAlt.Text = "Alt";
+            this.ckAlt.UseVisualStyleBackColor = true;
+            // 
+            // txtChar
+            // 
+            this.txtChar.Location = new System.Drawing.Point(12, 132);
+            this.txtChar.MaxLength = 1;
+            this.txtChar.Name = "txtChar";
+            this.txtChar.Size = new System.Drawing.Size(44, 20);
+            this.txtChar.TabIndex = 1;
+            this.txtChar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtChar.TextChanged += new System.EventHandler(this.TxtChar_TextChanged);
+            // 
+            // cmdSave
+            // 
+            this.cmdSave.Location = new System.Drawing.Point(62, 130);
+            this.cmdSave.Name = "cmdSave";
+            this.cmdSave.Size = new System.Drawing.Size(184, 23);
+            this.cmdSave.TabIndex = 2;
+            this.cmdSave.Text = "Save";
+            this.cmdSave.UseVisualStyleBackColor = true;
+            this.cmdSave.Click += new System.EventHandler(this.CmdSave_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "Testing";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 161);
+            this.Controls.Add(this.cmdSave);
+            this.Controls.Add(this.txtChar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Word Count";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox ckWindows;
+        private System.Windows.Forms.CheckBox ckShift;
+        private System.Windows.Forms.CheckBox ckControl;
+        private System.Windows.Forms.CheckBox ckAlt;
+        private System.Windows.Forms.TextBox txtChar;
+        private System.Windows.Forms.Button cmdSave;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ GlobalWordCount/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace GlobalWordCount
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info; //Shows the info icon so the user doesn't think there is an error.
+            notifyIcon1.Icon = new Icon(this.Icon, 40, 40);
+            notifyIcon1.Text = "Global Word Count Settings";
+
+            ckShift.Checked = true;
+            txtChar.Text = "W";
+
+            RegHotkey();
+        }
+
+        private void RegHotkey()
+        {
+            int Mod = 0;
+            if (ckAlt.Checked)
+                Mod += 1;
+            if (ckControl.Checked)
+                Mod += 2;
+            if (ckShift.Checked)
+                Mod += 4;
+            if (ckWindows.Checked)
+                Mod += 8;
+
+            KeysConverter kc = new KeysConverter();
+
+            RegisterHotKey(this.Handle, 0, Mod, kc.ConvertFromString(txtChar.Text).GetHashCode());
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x0312)
+            {
+                /* Note that the three lines below are not needed if you only want to register one hotkey.
+                 * The below lines are useful in case you want to register multiple keys, which you can use a switch with the id as argument, or if you want to know which key/modifier was pressed for some particular reason. 
+
+                Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);                  // The key of the hotkey that was pressed.
+                KeyModifier modifier = (KeyModifier)((int)m.LParam & 0xFFFF);       // The modifier of the hotkey that was pressed.
+                int id = m.WParam.ToInt32();                                        // The id of the hotkey that was pressed.*/
+
+                while (KeysDown())
+                    Thread.Sleep(50);
+
+                string prev = Clipboard.GetText(TextDataFormat.Text);
+
+                SendKeys.Send("^(c)");
+
+                string data = Clipboard.GetText(TextDataFormat.Text);
+
+                notifyIcon1.BalloonTipText = data.Replace(" ", string.Empty).Length + " chars" + "\n" + data.Split(' ').Length + " words";
+                notifyIcon1.BalloonTipTitle = "Word Count";
+
+                notifyIcon1.ShowBalloonTip(3000);
+
+                if(prev != null && prev != "")
+                    Clipboard.SetText(prev, TextDataFormat.Text);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, 0);
+        }
+
+        private void TxtChar_TextChanged(object sender, EventArgs e)
+        {
+            txtChar.Text = txtChar.Text.ToUpper();
+        }
+
+        private void CmdSave_Click(object sender, EventArgs e)
+        {
+            UnregisterHotKey(this.Handle, 0);
+            RegHotkey();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon1.BalloonTipText = "Global Word Count has been minimized";
+                notifyIcon1.BalloonTipTitle = "Information";
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(1000);
+                this.ShowInTaskbar = false;
+                RegHotkey();
+            }
+        }
+
+        private void NotifyIcon1_MouseDoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            notifyIcon1.Visible = false;
+            RegHotkey();
+        }
+
+        public static bool KeysDown()
+        {
+            foreach (Key key in Enum.GetValues(typeof(Key)))
+            {
+                try
+                {
+                    if (Keyboard.IsKeyDown(key))
+                        return true;
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            return false;   
+        }
+    }
+}
